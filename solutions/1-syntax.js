0a1b67217fe5767f971c8fe7ff6376c8cf0818fa
Rufeex <nemofeex@gmail.com>
2026-04-02 21:28:11 +0300

    kj

--- solutions/1-syntax.js
+++ solutions/1-syntax.js
@@ -1,3 +1,4 @@
 // BEGIN (write your solution here)
-export const getWeekends = (dlina = 'long') =>  dlina === 'short' ? ['sat', 'sun'] : ['saturday', 'sunday'];
+export const getWeekends = (dlina = 'long') =>  
+    dlina === 'short' ? ['sat', 'sun'] : ['saturday', 'sunday'];
 // END
